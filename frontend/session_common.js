34866f232217e4be52ad0b3bd6e27642a3e4b5d0
Aditya Goel <adityagoelyo@gmail.com>
2026-08-21 23:33:38 +0530

    halfway through making the page robust to refresh, offloaded many js functions to common file instead

--- frontend/session_common.js
+++ frontend/session_common.js
@@ -21,15 +21,22 @@ async function getHighestAttemptNumber(user, sessionProblems, current_problem_nu
     return HighestAttemptNumber
 }
 
-async function upload_attempt_number_to_session_attempts(sessionId, user, params, now) {
-    session_attempt_id = crypto.randomUUID();
-    await client.from('session_attempts').insert({
-        'id': session_attempt_id,
-        'session_id': sessionId,
-        'solver_id': user.id,
-        'attempt_number': parseInt(params.get('attempt'), 10),
-        'started_at': now
-    });
+async function upload_attempt_number_to_session_attempts(user, sessionId, user, params, now) {
+    let { data:existingAttempt, error } = await client.from('session_attempts').select('id').eq('session_id', sessionId).eq('solver_id', user.id).eq('attempt_number', parseInt(params.get('attempt'), 10)).maybeSingle();
+    let session_attempt_id;
+    if (existingAttempt) {
+        session_attempt_id = existingAttempt.id;
+    } else {
+        session_attempt_id = crypto.randomUUID();
+        await client.from('session_attempts').upsert({
+            'id': session_attempt_id,
+            'session_id': sessionId,
+            'solver_id': user.id,
+            'attempt_number': parseInt(params.get('attempt'), 10),
+            'started_at': now
+        });
+    }
+    return session_attempt_id;
 }
 
 function render_problem_title_id_and_counter(sessionProblems, current_problem_number){
@@ -58,4 +65,98 @@ function showAnswerBlock(problemType) {
     } else if (problemType === 'mcq') {
         document.getElementById('mcq_block').classList.remove('hidden');
     }
+}
+
+function markAsSubmitted(buttonEl) { 
+    buttonEl.disabled = true;
+    buttonEl.classList.remove('nav_button');
+    buttonEl.classList.add('dead_button');
+ }
+
+function markAsNotSubmitted(buttonEl) { 
+    buttonEl.disabled = false;
+    buttonEl.classList.add('nav_button');
+    buttonEl.classList.remove('dead_button');
+}
+ 
+async function refresh_answer_fields(proof_based_answer_button, isProved, problemStartedAt, problemEndedAt, sessionProblems, current_problem_number, mcq_options, mcq_submit, integer_submit){
+
+    // reset proof values
+    proof_based_answer_button.classList.remove('selected');
+    isProved = false;
+    problemStartedAt = new Date();
+    problemEndedAt = new Date();
+
+    // load mcq options
+    if (sessionProblems[current_problem_number].problems.problem_type == 'mcq') {
+        mcq_options.replaceChildren();
+        markAsNotSubmitted(mcq_submit);
+        let { data: options , error: optionsError } = await client.from('solutions')
+            .select('*')
+            .eq('problem_id', sessionProblems[current_problem_number].problems.id)
+            .eq('canon', true);
+
+        for (const option of options) {
+            const cell = document.createElement('button');
+            cell.className = 'subcell nav_button';
+            cell.innerHTML = option.statement_html;
+            cell.addEventListener('click', async function click() {
+                const previouslySelected = mcq_options.querySelector('.selected');
+                if (previouslySelected) previouslySelected.classList.remove('selected');
+                cell.classList.add('selected');
+            });
+            mcq_options.appendChild(cell)
+        }
+    } else if (sessionProblems[current_problem_number].problems.problem_type == 'integer') {
+        integer_submit.disabled = false;
+        integer_submit.classList.remove('dead_button');
+        integer_submit.classList.add('nav_button');
+    }
+    return { isProved, problemStartedAt, problemEndedAt };
+}
+
+async function submit_mcq_answer(mcq_submit, mcq_options, user, sessionProblems, current_problem_number, sessionId, solution_id, HighestAttemptNumber, problemStartedAt, problemEndedAt, session_attempt_id) {
+    solution_id = crypto.randomUUID();
+    markAsSubmitted(mcq_submit);
+    const selectedOption = mcq_options.querySelector('.selected');
+    const answer_text = selectedOption.statement_text;
+    const answer_html = selectedOption.statement_html;
+    await client.from('solutions').insert({
+        'id' : solution_id, 
+        'solver_id' : user.id,
+        'problem_id' : sessionProblems[current_problem_number].problems.id,
+        'canon' : false,
+        'statement_text' : answer_text,
+        'statement_html' : answer_html
+    });
+    await client.from('attempts').insert({
+        'solver_id':user.id,
+        'problem_id':sessionProblems[current_problem_number].problems.id,
+        'session_id':sessionId,
+        'solution_id': solution_id,
+        'attempt_number':HighestAttemptNumber+1, // correct this to a more dynamic number later
+        'time_taken_seconds': Math.round((problemEndedAt - problemStartedAt) / 1000), // time taken in seconds
+        'session_attempt_id': session_attempt_id
+    });
+}
+
+async function submit_integer_answer(integer_submit, user, sessionProblems, current_problem_number, sessionId, HighestAttemptNumber, problemStartedAt, problemEndedAt, session_attempt_id) {
+    solution_id = crypto.randomUUID();
+    markAsSubmitted(integer_submit);
+    await client.from('solutions').insert({
+        'id' : solution_id, 
+        'solver_id' : user.id,
+        'problem_id' : sessionProblems[current_problem_number].problems.id,
+        'canon' : false,
+        'statement_text' : document.getElementById('integer_answer_input').value
+    });
+    await client.from('attempts').insert({
+        'solver_id':user.id,
+        'problem_id':sessionProblems[current_problem_number].problems.id,
+        'session_id':sessionId,
+        'solution_id': solution_id,
+        'attempt_number':HighestAttemptNumber+1, // correct this to a more dynamic number later
+        'time_taken_seconds': Math.round((problemEndedAt - problemStartedAt) / 1000), // time taken in seconds
+        'session_attempt_id': session_attempt_id
+    });
 }
