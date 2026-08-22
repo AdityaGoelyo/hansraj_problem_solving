daba449f5da4203d0688077be35f22dafcec2274
Aditya Goel <adityagoelyo@gmail.com>
2026-08-22 23:34:52 +0530

    sent a lot of js into another file and segreted it into loading functions and updating functions

--- frontend/session_common.js
+++ frontend/session_common.js
@@ -6,7 +6,8 @@ async function loadData() {
     const { data: sessionProblems } = await client.from('session_problems').select('*, problems(*)').eq('session_id', sessionId).order('position');
     let current_problem_number = session.current_problem_number;
     let now = new Date().toISOString();
-    return { user, params, session, sessionId, sessionProblems, current_problem_number, now };
+    const total_num_problems = Math.max(...sessionProblems.map(item => item.position));
+    return { user, params, session, sessionId, sessionProblems, current_problem_number, now, total_num_problems};
 }
 
 async function getHighestAttemptNumber(user, sessionProblems, current_problem_number) {
@@ -18,10 +19,10 @@ async function getHighestAttemptNumber(user, sessionProblems, current_problem_nu
     .limit(1);
 
     let HighestAttemptNumber = PreviousAttemptNumber.length ? PreviousAttemptNumber[0].attempt_number : 0; 
-    return HighestAttemptNumber
+    return HighestAttemptNumber;
 }
 
-async function upload_attempt_number_to_session_attempts(user, sessionId, user, params, now) {
+async function upload_attempt_number_to_session_attempts(user, sessionId, params, now) {
     let { data:existingAttempt, error } = await client.from('session_attempts').select('id').eq('session_id', sessionId).eq('solver_id', user.id).eq('attempt_number', parseInt(params.get('attempt'), 10)).maybeSingle();
     let session_attempt_id;
     if (existingAttempt) {
@@ -37,20 +38,6 @@ async function upload_attempt_number_to_session_attempts(user, sessionId, user, 
         });
     }
     return session_attempt_id;
-}
-
-function render_problem_title_id_and_counter(sessionProblems, current_problem_number){
-    const problem_counter = document.getElementById("problem_counter");
-    const total_num_problems = Math.max(...sessionProblems.map(item => item.position));
-    problem_counter.textContent = `Current Problem Number: ${current_problem_number + 1}/${total_num_problems + 1}`;
-
-    const current_problem_title = document.getElementById("current_problem_title");
-    current_problem_title.textContent = sessionProblems[current_problem_number].problems.title;
-
-    const current_problem_id = document.getElementById("current_problem_id");
-    current_problem_id.textContent = `Problem Id: ${sessionProblems[current_problem_number].problems.id}`;
-
-    return {problem_counter, total_num_problems, current_problem_title, current_problem_id}
 }
 
 function showAnswerBlock(problemType) {
@@ -159,4 +146,58 @@ async function submit_integer_answer(integer_submit, user, sessionProblems, curr
         'time_taken_seconds': Math.round((problemEndedAt - problemStartedAt) / 1000), // time taken in seconds
         'session_attempt_id': session_attempt_id
     });
+}
+
+function define_const_elements() {
+    const proof_block = document.getElementById('proof_block');            
+    const mcq_options = document.getElementById("mcq_options");
+    const mcq_submit = document.getElementById('mcq_submit');
+    const integer_block = document.getElementById('integer_block');
+    const integer_submit = document.getElementById('integer_submit');
+    const proof_based_answer_button = document.getElementById("proof_based_answer_button");
+    const webpage_title = document.getElementById("webpage_title");
+    const sessionTitle = document.getElementById("sessionTitle");
+    const current_problem_data = document.getElementById("current_problem_data");
+    const problem_counter = document.getElementById("problem_counter");
+    const current_problem_title = document.getElementById("current_problem_title");
+    const current_problem_id = document.getElementById("current_problem_id");
+
+    return {proof_block, mcq_options, mcq_submit, integer_block, integer_submit, proof_based_answer_button, webpage_title, sessionTitle, current_problem_data, problem_counter, current_problem_title, current_problem_id};
+}
+
+function define_let_variables() {
+    let answer_text = ""
+    let answer_html = ""
+    let solution_id = crypto.randomUUID();
+    let problemStartedAt = new Date();
+    let problemEndedAt = new Date();
+    let isProved = false;
+    let previousProblemNumber;
+
+    return {answer_text, answer_html, solution_id, problemStartedAt, problemEndedAt, isProved, previousProblemNumber};
+}
+
+function update_problem_in_frontend(current_problem_number, payload, problem_counter, current_problem_title, current_problem_data, current_problem_id, sessionProblems, total_num_problems, previousProblemNumber) {
+    previousProblemNumber = current_problem_number;
+    current_problem_number = payload.new.current_problem_number;
+    problem_counter.textContent = `Current Problem Number: ${current_problem_number + 1}/${total_num_problems + 1}`;
+    current_problem_title.textContent = sessionProblems[current_problem_number].problems.title;
+    current_problem_data.innerHTML = sessionProblems[current_problem_number].problems.statement_html;
+    current_problem_id.textContent = `Problem Id: ${sessionProblems[current_problem_number].problems.id}`;
+
+    return {previousProblemNumber, current_problem_number};
+}
+
+async function submit_proof_answer(isProved, user, sessionProblems, problemNumber, sessionId, HighestAttemptNumber, problemEndedAt, problemStartedAt, session_attempt_id) {
+    if (isProved == false) problemEndedAt = new Date();
+    const error = await client.from('attempts').insert({
+        'solver_id':user.id,
+        'problem_id':sessionProblems[problemNumber].problems.id,
+        'session_id':sessionId,
+        'attempt_number':HighestAttemptNumber + 1,
+        'is_correct':isProved,
+        'time_taken_seconds': Math.round((problemEndedAt - problemStartedAt) / 1000), // time taken in seconds
+        'session_attempt_id': session_attempt_id
+    });
+    if (error) console.error(error);
 }
